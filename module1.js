83f93f4e96ab84942fc299b7c4c50dc371e68536
vishwa <karkavishwanath230@gmail.com>
2022-03-23 00:12:49 +0530

    importing and exporting modules

---
+++ module1.js
@@ -0,0 +1,9 @@
+let data ={
+    firstName:"Ramu",
+    lastName:"kumar",
+    rollno:45,
+    marks:80
+}
+let arr=[10,30,50]
+//module.exports = data,arr  // only data is exports arr is not exported there in no {} if we use {} it becomes one variable
+module.exports = {data,arr}
---
+++ module2.js
@@ -0,0 +1,6 @@
+let Data = require('./module1') // importing
+console.log(Data) 
+//{
+//    data: { firstName: 'Ramu', lastName: 'kumar', rollno: 45, marks: 80 },
+//    arr: [ 10, 30, 50 ]
+//  }
