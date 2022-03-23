6b0caad277c40cc190f28e410e227793d4d2b8f5
vishwa <karkavishwanath230@gmail.com>
2022-03-23 21:02:20 +0530

    modules

--- module1.js
+++ module1.js
@@ -5,5 +5,5 @@ let data ={
     marks:80
 }
 let arr=[10,30,50]
-//module.exports = data,arr  // only data is exports arr is not exported there in no {} if we use {} it becomes one variable
+//module.exports = data,arr  // only data is exports arr is not exported there in no {}, if we use {} it becomes one variable
 module.exports = {data,arr}
