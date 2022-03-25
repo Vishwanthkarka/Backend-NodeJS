623b96a3d5f86be90918c6c4469e692043bd3a0c
vishwa <karkavishwanath230@gmail.com>
2022-03-25 19:07:47 +0530

    without express

--- server.js
+++ server.js
@@ -4,22 +4,44 @@ const http = require('http')
 // create http server
 
 // the purpose of the server is it should listen the client request and it should send back the response to the client
- const server = http.createServer((request,response)=> {// request call back function  // two argument
+ const server = http.createServer((request,response)=> { // request call back function  // two argument
  // which takes request object and which can response object 
 console.log(request.method)  // to find request type
  //server with response.end("This is response from http server")
-if (request.method == "GET"){
-    response.end("THIS IS AN GET ")
-}
-if (request.method == "PUT"){
-    response.end("THIS IS AN PUT ")
-}
-if (request.method == "POST"){
-    response.end("THIS IS AN POST ")
-}
-if (request.method == "DELETE"){
-    response.end("THIS IS AN DELETE ")
-}
+  //GET req
+  if(request.method=='GET'){
+    if(request.url==='/getusers'){
+      response.end("This response for USERS")
+    }
+    if(request.url==='/getproducts'){
+      response.end("This response for PRODUCTS")
+    }
+  }
+
+
+  //POST req
+  if(request.method=='POST'){
+   
+    if(request.url=='/createuser'){
+      response.end("User created")
+    }
+
+    if(request.url=='/createproduct'){
+      response.end("Product created")
+    }
+  }
+
+ //PUT req
+ if(request.method=='PUT'){
+    response.end("This response for PUT request")
+  }
+  
+  //DELETE req
+  if(request.method=='DELETE'){
+    response.end("This response for DELETE request")
+  }
+
+
 })
 //  client => HTTP Request   Server 
 // client  <=  HTTP Response  Server
@@ -31,6 +53,9 @@ if (request.method == "DELETE"){
 // DELETE => delete
 
  // every server should have the port number to  listen client request 
+
+ 
+
 
  //assign port number 
  server.listen(3000,()=> console.log("server listening on port 3000. "))
